febc7c356bdae51d1a4c28813bea886644792da6
abel <abel_zhu@msn.com>
2020-03-02 07:38:29 +0800

    init project

---
+++ src/main/java/com/example/springaccessingmysql/SpringAccessingMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaccessingmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAccessingMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAccessingMysqlApplication.class, args);
+	}
+
+}
